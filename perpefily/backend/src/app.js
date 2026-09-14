758860dd74c20b93dcff73ef5ba14c28115c2151
rohit2902 <rohitabrssm@gmail.com>
2026-09-14 22:58:21 +0530

    commit

--- perpefily/backend/src/app.js
+++ perpefily/backend/src/app.js
@@ -98,7 +98,7 @@ app.get("/", (req, res) => {
 });
 
 // Handle undefined routes
-app.use("*", (req, res) => {
+app.use("*name", (req, res) => {
   res.status(404).json({ success: false, message: "API Route Not Found" });
 });
 
